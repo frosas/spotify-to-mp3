58e14904e07f8533fb46b708c0fa372923c6d7db
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-08-19 13:42:12 +0200

    Move App class to its own file

---
+++ lib/spotify_to_mp3/app.rb
@@ -0,0 +1,40 @@
+module SpotifyToMp3
+  class App
+    def initialize
+      @context = {
+        :grooveshark => Grooveshark.new,
+        :spotify => Spotify.new
+      }
+    end
+
+    def run
+      file = ARGV.first
+      raise "No songs file specified. Usage: #{$0} file" if file.nil?
+
+      File.open(file).each_line{|song_id|
+        song_id.strip!
+        next if song_id.empty?
+        begin
+          song = UnresolvedSong.new(@context, song_id)
+          print "Resolving \"#{song}\" "
+          song = song.resolve
+          print "-> Searching \"#{song}\" on Grooveshark "
+          song = song.from_grooveshark
+          print "-> Downloading \"#{song}\" "
+          if File.exists? song.filename
+            FileUtils.touch song.filename # To know about songs no longer in download list
+            puts "Already exists, skipping".green
+          else
+            song.download
+            puts "Done".green
+          end
+        rescue Exception => exception # For some reason without the "Exception" it is ignored
+          puts exception.message.red
+          # Continue with the next track
+        end
+      }
+    rescue
+      puts "#{$!}".red
+    end
+  end
+end
