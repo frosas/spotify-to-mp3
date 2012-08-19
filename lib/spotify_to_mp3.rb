2858da9fd1ade22cd87a7d75ec879c54f2e8d68f
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-08-19 13:40:49 +0200

    Refact

--- lib/spotify_to_mp3.rb
+++ lib/spotify_to_mp3.rb
@@ -21,38 +21,33 @@ module SpotifyToMp3
     end
 
     def run
-      begin
-        file = ARGV.first
-        raise "No songs file specified. Usage: #{$0} file" if file.nil?
-        
-        File.open(file).each_line{|song_id|
-          song_id.strip!
-          next if song_id.empty?
-          begin
-            song = UnresolvedSong.new(@context, song_id)
-            print "Resolving \"#{song}\" "
-            song = song.resolve
-            print "-> Searching \"#{song}\" on Grooveshark "
-            song = song.from_grooveshark
-            print "-> Downloading \"#{song}\" "
-            if File.exists? song.filename
-              FileUtils.touch song.filename # To know about songs no longer in download list
-              puts "Already exists, skipping".green
-            else
-              begin
-                song.download
-                puts "Done".green
-              rescue Exception => exception
-                puts exception.message.red
-              end
-            end
-          rescue
-            puts "#{$!}".red
+      file = ARGV.first
+      raise "No songs file specified. Usage: #{$0} file" if file.nil?
+
+      File.open(file).each_line{|song_id|
+        song_id.strip!
+        next if song_id.empty?
+        begin
+          song = UnresolvedSong.new(@context, song_id)
+          print "Resolving \"#{song}\" "
+          song = song.resolve
+          print "-> Searching \"#{song}\" on Grooveshark "
+          song = song.from_grooveshark
+          print "-> Downloading \"#{song}\" "
+          if File.exists? song.filename
+            FileUtils.touch song.filename # To know about songs no longer in download list
+            puts "Already exists, skipping".green
+          else
+            song.download
+            puts "Done".green
           end
-        }
-      rescue
-        puts "#{$!}".red
-      end
+        rescue Exception => exception # For some reason without the "Exception" it is ignored
+          puts exception.message.red
+          # Continue with the next track
+        end
+      }
+    rescue
+      puts "#{$!}".red
     end
   end
 end
