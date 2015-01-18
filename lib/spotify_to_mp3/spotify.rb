707ac67e48b4a487b65dc74088f3e95c70c077e6
osboxes.org <martin.dulak@gmail.com>
2015-01-18 16:35:34 +0000

    Bugfix of Spotify HTTP URL search. Original commit: fc1b5f7c9e34aeb45255af32ac485113da6bb0fd

--- lib/spotify_to_mp3/spotify.rb
+++ lib/spotify_to_mp3/spotify.rb
@@ -5,6 +5,8 @@ module SpotifyToMp3
   class Spotify
     def get_track(uri)
       track_id = uri.sub(/.*:/, '')
+      track_id = track_id.sub(/.*\//, '') if uri.start_with?('http')
+
       track = RSpotify::Track.find(track_id)
       Track.new(track.artists.first.name, track.name)
     end
