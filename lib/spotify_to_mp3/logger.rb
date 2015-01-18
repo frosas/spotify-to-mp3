f726752a6fb8f9c3c666f6d3c6ef2dd154094ed4
osboxes.org <martin.dulak@gmail.com>
2015-01-18 21:24:51 +0000

    Added more convenient logging and interface for batch processing. Download and resolve phases are now divided. Downloading starts as soon as everything from IStream was read.

--- lib/spotify_to_mp3/logger.rb
+++ lib/spotify_to_mp3/logger.rb
@@ -10,11 +10,19 @@ module SpotifyToMp3
       @io_stream.puts str
     end
 
+    def succ(str)
+      @io_stream.puts colorize_succ(str)
+    end
+
     def error(str)
       @io_stream.puts colorize_error(str)
     end
 
     private
+
+    def colorize_succ(str)
+      str.green
+    end
 
     def colorize_error(str)
       str.red
