59c52049ab93652acc8ceeb8e352b231c98417be
osboxes.org <martin.dulak@gmail.com>
2015-01-18 15:49:22 +0000

    Showing download progress.

--- lib/spotify_to_mp3/grooveshark.rb
+++ lib/spotify_to_mp3/grooveshark.rb
@@ -1,5 +1,5 @@
-require 'fileutils'
-require 'rest-client'
+require 'net/http'
+require 'ruby-progressbar'
 require 'spotify_to_mp3/grooveshark/track'
 
 module SpotifyToMp3
@@ -15,8 +15,29 @@ module SpotifyToMp3
 
     def download(track)
       url = @client.get_song_url(track.client_track)
-      file = RestClient::Request.execute(:method => :post, :url => url, :raw_response => true).file
-      FileUtils.mv(file.path, track.filename)
+      uri = URI(url)
+
+      Net::HTTP.start(uri.host) do |http|
+        http.request_post("#{uri.path}?#{uri.query}", "") do |response|
+          half_win = $stdin.winsize[1] / 2
+          title = track.to_s[0..half_win.pred].ljust half_win
+          title = title.gsub(/.{3}$/, '...') if track.to_s.length > half_win
+
+          pbar = ProgressBar.create(
+            :title => title,
+            :total => response['content-length'].to_i,
+            :format => '%t %p%% [%B] %E')
+
+          File.open(track.filename, 'w') do |f|
+            response.read_body do |str|
+              f.write str
+              str.length.times { pbar.increment }
+            end
+          end
+
+          pbar.finish
+        end
+      end
     end
   end
 end
